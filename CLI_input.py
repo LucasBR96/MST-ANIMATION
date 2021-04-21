d31a99fc0d1b0f99e209e6838e3ec11ffea747a7
LucasBR96 <lucas_cipriano@id.uff.br>
2021-04-21 19:24:50 -0300

    CLI pronto

--- CLI_input.py
+++ CLI_input.py
@@ -2,13 +2,7 @@ import string
 
 # GLOBAL VARS --------------------------------------------------
 EXIT_CHAR = "*"
-
-# KRUSKAL = 0
-# PRIM    = 1
-
-# RANDOM  = 0
-# CUSTOM  = 1
-
+END_CHAR = "-1"
 MAX_NODES = 100
 
 input_info = dict()
@@ -57,6 +51,27 @@ def mode_choice():
             print( "entrada invalida, digite novamente")
     
     print()
+
+def exec_choice():
+
+    print( "escolha a execução:" )
+    print( "0 - direta" ) # Runs a step after the other, pausing after pressing enter
+    print( "1 - iterativa" ) # Runs a step each time the key 'enter' is pressed
+
+    while True:
+
+        c = input()
+        if c == EXIT_CHAR: raise InterruptedError
+
+        if c == "0":
+            input_info[ "exec" ] = "DIRECT"
+            break
+        elif c == "1":
+            input_info[ "exec" ] = "ITERACT"
+            break
+        else:
+            print( "entrada invalida, digite novamente")
+
 def random_build():
 
     # setting number of nodes
@@ -73,13 +88,13 @@ def random_build():
                 input_info[ "num_nodes" ] = m
                 break
         print( "entrada invalida, digite novamente")
+    print()
     
     min_edges = m - 1    # A tree, Basicaly
     max_edges = m**2 - m # Fully connected
     print("digite a quantidade de arestas")
     print("maximo -> {}".format( max_edges ) ) 
     print("minimo -> {}".format( min_edges ) )
-
     while True:
 
         c = input()
@@ -91,11 +106,48 @@ def random_build():
                 break
         print( "entrada invalida, digite novamente")  
     print()
-    
+
+def custom_build( ):
+
+    print( "digite os vertices do grafo" )
+    print( 'formato: m m i' )
+    print( "m -> minuscula")
+    print( "i -> inteiro" )
+    print( "digite -1 se acabou")
+
+    input_info[ "nodes" ] = set()
+    input_info[ "edges" ] = dict()
+    while True:
+        tup = input().rstrip()
+        if tup == END_CHAR:
+            break
+        elif tup == EXIT_CHAR: raise InterruptedError
+
+        s = tup.split()
+        if len( s ) != 3 or s[-1] not in string.digits:
+            print( "entrada invalida, digite novamente") 
+        
+        a , b , c = s
+        input_info[ "edges" ][ ( a , b ) ] = int( c )
+        input_info[ "nodes" ].add( a )
+        input_info[ "nodes" ].add( b )
+    print()
+
+def get_user_input():
+
+    try:
+        algo_choice()
+        mode_choice()
+        exec_choice()
+        if input_info["build"] == "RANDOM":
+            random_build()
+        elif input_info["build"] == "CUSTOM":
+            custom_build()
+        
+        print( *input_info.items() , sep = "\n")
+
+    except InterruptedError:
+        return
+
 if __name__ == "__main__":
-    algo_choice()
-    mode_choice()
-    if input_info["build"] == "RANDOM":
-        random_build()
-    
-    print( *input_info.items() , sep = "\n")
+    get_user_input()
